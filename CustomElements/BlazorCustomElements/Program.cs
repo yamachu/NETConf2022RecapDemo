f707b83e029fbed4a8bb50b1c389b35794aaada4
yamachu <yamachu.dev@gmail.com>
2022-12-02 23:55:52 +0900

    Register Counter component as CustomElement

--- CustomElements/BlazorCustomElements/Program.cs
+++ CustomElements/BlazorCustomElements/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorCustomElements;
+using BlazorCustomElements.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.RegisterCustomElement<Counter>("blazor-counter");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
